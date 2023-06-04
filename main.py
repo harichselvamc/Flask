8f0f361f4ae8dbd40d60c409e98dc3e6f02975e9
harichselvam <harichselvamc@gmail.com>
2023-06-04 08:18:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,200 @@
-import uvicorn
-import cv2
-import os
-from typing import List
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
+import streamlit as st
+from PIL import Image, ImageDraw, ImageFont
+import io
+import json
+import base64
+import pyperclip
 
-app = FastAPI()
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+def add_image_overlay(images, image_data_list):
+    images_with_overlay = []
+
+    for image, image_data in zip(images, image_data_list):
+        img = Image.open(io.BytesIO(image))
+        overlay = Image.new('RGBA', img.size)
+
+        image_name = image_data['image_name']
+        font_size = image_data['font_size']
+        position = image_data['position']
+        text_color = image_data['text_color']
+        font = ImageFont.truetype('./arial.ttf', font_size)
+
+        if position == 'bottom-left':
+            x = 10
+            y = img.height - font_size - 10
+        elif position == 'bottom-right':
+            text_width, _ = font.getsize(image_name)
+            x = img.width - text_width - 10
+            y = img.height - font_size - 10
+        else:
+            x = 10
+            y = 10
+
+        draw = ImageDraw.Draw(overlay)
+        draw.text((x, y), image_name, font=font, fill=text_color)
+
+        img_with_overlay = Image.alpha_composite(img.convert('RGBA'), overlay)
+        images_with_overlay.append(img_with_overlay)
+
+    return images_with_overlay
+
 
 def resize_image(image, size):
-    width, height = size.split('x')
-    width = int(width)
-    height = int(height)
-    return cv2.resize(image, (width, height))
+    width, height = size
+    return image.resize((width, height), resample=Image.LANCZOS)
 
-@app.post('/overlay-text')
-async def overlay_text(files: List[UploadFile] = File(...), image_size: str = '1080x1080'):
-    # If no files are uploaded
-    if len(files) == 0:
-        return {'error': 'No files uploaded'}
 
-    output_directory = os.path.join(os.getcwd(), 'output')
-    os.makedirs(output_directory, exist_ok=True)  # Directory allocation
+def store_json_data(json_data):
+    with open('history.json', 'a') as file:
+        file.write(json_data + '\n')
 
-    download_urls = []  # List of URLs
-    for i, file in enumerate(files):
-        temp_path = f'temp_{i}.png'
-        with open(temp_path, 'wb') as temp_file:
-            temp_file.write(await file.read())  # Saving the file to a temporary location
 
-        # Loading the image
-        image = cv2.imread(temp_path)
+def load_json_data():
+    data = []
+    with open('history.json', 'r') as file:
+        for line in file:
+            data.append(json.loads(line))
+    return data
 
-        if image is None: # If the image fails to load
-            os.remove(temp_path)
-            return {'error': f'Failed to load image {i}'}
 
-        # Resize the image
-        image = resize_image(image, image_size)
+def save_to_history(data):
+    image_name = data['image_name']
+    font_size = data['font_size']
+    position = data['position']
+    text_color = data['text_color']
+    altered_size = data.get('altered_size', None)
 
-        # Extracting the file name from the uploaded file
-        file_name = file.filename
-        file_name_without_extension, file_extension = os.path.splitext(file_name)
+    image_data = {
+        'image_name': image_name,
+        'font_size': font_size,
+        'position': position,
+        'text_color': text_color,
+        'altered_size': altered_size
+    }
 
-        # Prepare the text to overlay on the image
-        text = f'{file_name_without_extension}{file_extension}'
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 2
-        font_thickness = 5
-        text_color = (0, 0, 238)
+    store_json_data(json.dumps(image_data))
+    return {"message": "Data saved to history.json"}
 
-        # Set the text size
-        (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, font_thickness)
 
-        # Calculate the position to place the text
-        x = int((image.shape[1] - text_width) / 2) # Centered horizontally
-        y = int((image.shape[0] + text_height) - 30)  # Centered vertically
+@app.route('/api/history', methods=['GET'])
+def get_history():
+    history_data = load_json_data()
+    return jsonify(history_data)
 
-        # Overlay the text on the image
-        cv2.putText(image, text, (x, y), font, font_scale, text_color, font_thickness, cv2.LINE_AA)
 
-        # Set the output file path
-        output_file_path = os.path.join(output_directory, f'output_{i}.png')
+@app.route('/api/image-overlay', methods=['POST'])
+def process_image_overlay():
+    uploaded_files = request.files.getlist('files')
+    image_data_list = json.loads(request.form.get('data'))
 
-        # Save the resulting image
-        cv2.imwrite(output_file_path, image)
+    images = [uploaded_file.read() for uploaded_file in uploaded_files]
+    images_with_overlay = add_image_overlay(images, image_data_list)
 
-        # Remove the temporary file
-        os.remove(temp_path)
+    response_data = []
 
-        # Generate the download link URL for the output image
-        download_url = f'/download/output_{i}.png'
-        download_urls.append(download_url)
+    for i, image_with_overlay in enumerate(images_with_overlay):
+        resized_image_with_overlay = resize_image(image_with_overlay, (800, 600))
+        image_path = f"image_{i+1}.png"
+        resized_image_with_overlay.save(image_path, 'PNG')
 
-    # Return the download URLs in the API response
-    return {'download_urls': download_urls}
+        with open(image_path, 'rb') as file:
+            image_data = file.read()
 
-@app.get('/download/{filename}')
-async def download(filename: str):
-    # Making the file path
-    file_path = os.path.join(os.getcwd(), 'output', filename)
+        image_base64 = base64.b64encode(image_data).decode('utf-8')
 
-    # Check if the file exists
-    if os.path.isfile(file_path):
-        # Returning the file as a downloadable response
-        return FileResponse(file_path, filename=filename)
+        response_data.append({
+            'image_name': image_data_list[i]['image_name'],
+            'image': image_base64
+        })
 
-    # If the file doesn't exist, this will return a 404 Not Found error
-    return {'error': 'File not found'}
+    return jsonify(response_data)
+
+
+def main():
+    st.title('Image Overlay API')
+
+    uploaded_files = st.file_uploader('Upload images', type=['png', 'jpg', 'jpeg'], accept_multiple_files=True)
+    if uploaded_files:
+        image_data_list = []
+        images = [uploaded_file.read() for uploaded_file in uploaded_files]
+
+        for i, uploaded_file in enumerate(uploaded_files):
+            image_name = st.text_input(f'Enter image name for Image {i+1}', value=uploaded_file.name)
+            font_size = st.number_input(f'Overlay font size for Image {i+1}', min_value=1, value=20)
+            position = st.selectbox(f'Overlay position for Image {i+1}', ('bottom-left', 'bottom-right', 'top-left', 'top-right'))
+            text_color = st.color_picker(f'Text color for Image {i+1}', '#FFFFFF')
+
+            image_data_list.append({
+                'image_name': image_name,
+                'font_size': font_size,
+                'position': position,
+                'text_color': text_color
+            })
+
+        images_with_overlay = add_image_overlay(images, image_data_list)
+
+        for i, image_with_overlay in enumerate(images_with_overlay):
+            st.image(image_with_overlay, caption=f'Image {i+1} with Overlay', use_column_width=True)
+
+            if st.checkbox(f"Resize Image {i+1}"):
+                unique_key = f"resize_select_{i}"
+                image_size = st.selectbox("Select Image Size", (
+                    "Default", "1920x1080", "630x900", "720x1080", "900x1260",
+                    "1080x1440", "1440x1800", "1530x1980", "1800x2520", "1050x1500",
+                    "1200x1800", "1500x2100", "1800x2400", "2400x3000", "2550x3300",
+                    "2800x3920", "3025x3850"
+                ), key=unique_key)
+
+                if image_size == "Default":
+                    resized_image_with_overlay = image_with_overlay
+                    altered_size = "Default"
+                else:
+                    width, height = image_size.split("x")
+                    width = int(width)
+                    height = int(height)
+                    resized_image_with_overlay = resize_image(image_with_overlay, (width, height))
+                    altered_size = image_size
+
+                st.image(resized_image_with_overlay, caption=f"Resized Image {i+1}", use_column_width=True)
+
+                image_path = f"image_{i+1}.png"
+                resized_image_with_overlay.save(image_path, 'PNG')
+                st.download_button(label=f'Download Image {i+1}', data=open(image_path, 'rb'), file_name=image_path,
+                                   mime='image/png')
+
+                image_data_list[i]['altered_size'] = altered_size
+
+        if st.button('Save'):
+            for i, image_data in enumerate(image_data_list):
+                image_data['image'] = base64.b64encode(images[i]).decode('utf-8')
+                save_to_history(image_data)
+            st.success('Data saved to history.json')
+
+        st.subheader('API Response')
+        st.json(image_data_list)
+
+        st.subheader('History Data')
+        history_data = load_json_data()
+        for data in history_data:
+            st.write(data)
+
+        api_link = "https://your-domain.com/api/history"  # Replace with the actual domain name
+        st.subheader("API Endpoint")
+        st.markdown(f"**API Link:** `{api_link}`")
+
+        st.subheader("Postman Commands")
+        st.markdown("To run the API on the local host, use the following command:")
+        st.code("postman run <your-collection>.json --global-var \"api_link=http://localhost:5000/api/history\"")
+
+        st.markdown("To run the API on the hosted site, use the following command:")
+        st.code("postman run <your-collection>.json --global-var \"api_link=https://your-domain.com/api/history\"")
+
+        st.markdown(f"[Click here]({api_link}) to copy the API Link to clipboard.")
+        pyperclip.copy(api_link)
+
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    main()
