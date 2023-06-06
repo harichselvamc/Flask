6aeab5de3082fa4d7cff3b4460ea98c832e410cd
harichselvam <harichselvamc@gmail.com>
2023-06-06 22:40:58 +0530

    Update ubu.py

--- ubu.py
+++ ubu.py
@@ -79,19 +79,18 @@ async def upload(files: List[UploadFile] = File(...), text: str = None):
     instructions = []
 
     for i, file in enumerate(files):
-        # Save the uploaded file temporarily
+       
         temp_path = os.path.join(output_directory, f"temp_{i}.png")
         with open(temp_path, "wb") as f:
             f.write(await file.read())
 
-        # Read the image using OpenCV
+     
         image = cv2.imread(temp_path)
 
-        # Get the name from the uploaded file
+        
         file_name = file.filename
         file_name_without_extension, file_extension = os.path.splitext(file_name)
 
-        # Prepare the text to overlay on the image
         if text:
             overlay_text = text
         else:
@@ -102,52 +101,51 @@ async def upload(files: List[UploadFile] = File(...), text: str = None):
         font_thickness = 5
         text_color = (0, 0, 238)
 
-        # Set the text size
         (text_width, text_height), _ = cv2.getTextSize(
             overlay_text, font, font_scale, font_thickness
         )
 
-        # Calculate the position to place the text
-        x = int((image.shape[1] - text_width) / 2)  # Centered horizontally
-        y = int((image.shape[0] + text_height) - 30)  # Centered vertically
+        
+        x = int((image.shape[1] - text_width) / 2)  
+        y = int((image.shape[0] + text_height) - 30)  
 
-        # Overlay the text on the image
+        
         cv2.putText(
             image, overlay_text, (x, y), font, font_scale, text_color, font_thickness, cv2.LINE_AA
         )
 
-        # Set the output file path
+    
         output_file_path = os.path.join(output_directory, f"output_{i}.png")
 
-        # Save the resulting image
+       
         cv2.imwrite(output_file_path, image)
 
-        # Remove the temporary file
+        
         os.remove(temp_path)
 
-        # Generate the download link URL for the output image
+      
         download_url = f"/static/output/output_{i}.png"
         download_urls.append(download_url)
 
-        # Add instruction
+       
         instruction = f"Image {file_name} processed. Download: <a href='{download_url}'>{file_name}</a>"
         instructions.append(instruction)
 
-    # Return the download URLs and instructions in the API response
+    
     return {"download_urls": download_urls, "instructions": instructions}
 
 
 @app.get("/download/{filename}")
 async def download(filename: str):
-    # Making the file path
+ 
     file_path = os.path.join("static", "output", filename)
 
-    # Check if the file exists
+ 
     if os.path.isfile(file_path):
-        # If the file exists, return it as a response
+        
         return FileResponse(file_path)
 
-    # If the file does not exist, return a 404 Not Found response
+    
     return {"detail": "File not found"}
 
 
@@ -165,6 +163,6 @@ async def get_overlayed_image_data():
     return {"images": image_data}
 
 
-# Start the server
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=12000)
