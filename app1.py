d6ebf5337754ee0b9a7cb9f9228eb72c59372bab
harichselvam <harichselvamc@gmail.com>
2023-06-07 17:05:18 +0530

    123

--- app1.py
+++ app1.py
@@ -1,85 +1,45 @@
 import os
 import cv2
-import numpy as np
 from typing import List
 from fastapi import FastAPI, UploadFile, Request, File
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
-from rembg import remove
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-def create_static_folder():
-    base_directory = os.getcwd()
-    static_directory = os.path.join(base_directory, "static")
-    overlayed_images_directory = os.path.join(static_directory, "overlayed_images")
-
-    print("Base Directory:", base_directory)
-    print("Static Directory:", static_directory)
-    print("Overlayed Images Directory:", overlayed_images_directory)
-
-    if not os.path.exists(static_directory):
-        os.makedirs(static_directory)
-        print("Static Directory Created.")
-
-    if not os.path.exists(overlayed_images_directory):
-        os.makedirs(overlayed_images_directory)
-        print("Overlayed Images Directory Created.")
-
-
-def get_next_file_number():
-    overlayed_images = os.listdir("static/overlayed_images")
-    file_numbers = [
-        int(file.split("_")[1].split(".")[0])
-        for file in overlayed_images
-        if file.startswith("output_")
-    ]
-    if file_numbers:
-        return max(file_numbers) + 1
-    else:
-        return 0
-
-
+# Define the home page
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 
+# Define the upload endpoint
 @app.post("/upload")
 async def upload(files: List[UploadFile] = File(...), text: str = None):
-    output_directory = "static/overlayed_images"
+    output_directory = "static/output"
     os.makedirs(output_directory, exist_ok=True)
 
     download_urls = []
     instructions = []
 
-    next_file_number = get_next_file_number()
-
     for i, file in enumerate(files):
-     
+        # Save the uploaded file temporarily
         temp_path = os.path.join(output_directory, f"temp_{i}.png")
         with open(temp_path, "wb") as f:
             f.write(await file.read())
 
-      
-        with open(temp_path, "rb") as image_file:
-            image_data = image_file.read()
-            output_data = remove(image_data)
+        # Read the image using OpenCV
+        image = cv2.imread(temp_path)
 
-        image_array = np.frombuffer(output_data, np.uint8)
-
-        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-        image = cv2.resize(image, (1080, 1080))
-
-       
+        # Get the name from the uploaded file
         file_name = file.filename
         file_name_without_extension, file_extension = os.path.splitext(file_name)
 
+        # Prepare the text to overlay on the image
         if text:
             overlay_text = text
         else:
@@ -90,51 +50,58 @@ async def upload(files: List[UploadFile] = File(...), text: str = None):
         font_thickness = 5
         text_color = (0, 0, 238)
 
+        # Set the text size
         (text_width, text_height), _ = cv2.getTextSize(
             overlay_text, font, font_scale, font_thickness
         )
 
-        x = int((image.shape[1] - text_width) / 2)  
-        y = image.shape[0] - int(text_height) - 30  
+        # Calculate the position to place the text
+        x = int((image.shape[1] - text_width) / 2)  # Centered horizontally
+        y = int((image.shape[0] + text_height) - 30)  # Centered vertically
 
+        # Overlay the text on the image
         cv2.putText(
             image, overlay_text, (x, y), font, font_scale, text_color, font_thickness, cv2.LINE_AA
         )
 
-     
-        output_file_path = os.path.join(output_directory, f"output_{next_file_number}.png")
+        # Set the output file path
+        output_file_path = os.path.join(output_directory, f"output_{i}.png")
+
+        # Save the resulting image
         cv2.imwrite(output_file_path, image)
 
-        download_url = f"/static/overlayed_images/output_{next_file_number}.png"
+        # Remove the temporary file
+        os.remove(temp_path)
+
+        # Generate the download link URL for the output image
+        download_url = f"/static/output/output_{i}.png"
         download_urls.append(download_url)
 
+        # Add instruction
         instruction = f"Image {file_name} processed. Download: {download_url}"
         instructions.append(instruction)
 
-        next_file_number += 1
-
+    # Return the download URLs and instructions in the API response
     return {"download_urls": download_urls, "instructions": instructions}
-
-
-
-create_static_folder()
 
 
 @app.get("/download/{filename}")
 async def download(filename: str):
+    # Making the file path
+    file_path = os.path.join("static", "output", filename)
 
-    file_path = os.path.join("static", "overlayed_images", filename)
-
+    # Check if the file exists
     if os.path.isfile(file_path):
-    
+        # If the file exists, return it as a response
         return FileResponse(file_path)
 
+    # If the file does not exist, return a 404 Not Found response
     return {"detail": "File not found"}
 
 
 @app.get("/data")
 async def get_data():
-    overlayed_images = os.listdir("static/overlayed_images")
+    overlayed_images = os.listdir("static/output")
     return {"overlayed_images": overlayed_images}
 
 
@@ -146,12 +113,13 @@ async def get_shareable_link(request: Request):
 
 @app.get("/images")
 async def get_images():
-    overlayed_images = os.listdir("static/overlayed_images")
+    overlayed_images = os.listdir("static/output")
     image_urls = [
-        f"http://localhost:12000/static/overlayed_images/{image}" for image in overlayed_images
+        f"http://localhost:12000/static/output/{image}" for image in overlayed_images
     ]
     return {"image_urls": image_urls}
 
 
+# Start the server
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=12000)
